35b031fdb3fc3147111dbc0fe51fcdc79ff6a0f8
felipeoli7eira <me.felipeoliveira@gmail.com>
2020-11-19 22:29:27 -0300

    Modificacao nas configs

--- source/boot/configs.php
+++ source/boot/configs.php
@@ -1,30 +1,17 @@
 <?php
 
-    /**
-     * URL
-    */
+    /** URL */
 
     define("CONF_URL_DEV", "http://localhost/jalbum");
     define("CONF_URL_PROD", "https://app.com.br");
 
-    /**
-     * RESOURCES
-    */
-
-    define("CONF_RESOURCES_DIR", "resources/");
-
-    /**
-     * VIEWS
-    */
+    /** VIEWS */
 
     define("CONF_VIEWS_DIR", __DIR__ . "/../../views");
     define("CONF_VIEWS_EXT", "php");
-    define("CONF_DEFAULT_PAGE_TITLE", "***");
-    define("CONF_FRAMEWORK_CSS_PATH", "");
-    define("CONF_FRAMEWORK_JS_PATH", "");
 
-    /**
-     * APP
-    */
+    /** APP */
 
     define("CONF_APP_NAME", "Jalbum");
+    define("CONF_DEFAULT_PAGE_TITLE", "***");
+    define("CONF_RESOURCES_DIR", "resources/");
